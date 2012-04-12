bd5de9c2c3dfed1c0adbce183bb4d302176ec24b
Luke Feng <windboxx@gmail.com>
2012-04-12 21:53:11 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WindApp::Application.config.secret_token = 'b108c1d8a80fc2dc64da2e76156ef2addaaad67cbf9101ac31eeab5754a7811614d525226aaec1cfde5fb004a2e79a5e522d4f97b1ff51fb5695fb037220be96'
